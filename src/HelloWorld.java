5d350016c53f544cb97f7654d991eb1285dbf4ae
Diana0422 <daylight@live.it>
2021-03-13 15:57:00 +0100

    HelloWorld.java class added.

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Starting main...");
+		System.out.println("Hello World!");
+		System.out.println("Shutting main...");
+	}
+}
